607b30fa8e27ab002c4e2a1627a9f38131d0cae3
Orlando <orlandoaviles99@gmail.com>
2026-01-30 08:03:39 -0500

    Rebuild of Creative Dojo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-import { initSpaceBackground } from "./space-bg.js";
-import { initBookmarks } from "./bookmarks.js";
+async function loadComponent(id, file) {
+  const res = await fetch(file);
+  const html = await res.text();
+  document.getElementById(id).innerHTML = html;
+}
 
-initSpaceBackground();
-initBookmarks();
+loadComponent("site-header", "components/header.html");
+loadComponent("site-footer", "components/footer.html");
+loadComponent("hero", "components/hero.html");
+loadComponent("dojo", "components/dojo.html");
+loadComponent("vfa", "components/vfa.html");
+loadComponent("projects", "components/projects.html");
+loadComponent("about", "components/about.html");
